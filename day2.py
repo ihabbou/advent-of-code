e2ccba22fb765bbfe513ae92bb1b7c409848e084
ihabbou <10790679+ihabbou@users.noreply.github.com>
2021-12-04 13:21:00 +0100

    day 2 part 2

--- day2.py
+++ day2.py
@@ -1,4 +1,6 @@
-# %% Day 2
+# https://adventofcode.com/2021/day/2
+
+# %% Day 2 part 1
 
 
 def move(instructions, position=(0, 0)):
@@ -7,5 +9,40 @@ def move(instructions, position=(0, 0)):
     horizontal = [int(val) for move, val in moves if move == "forward"]
     depth = [(int(val) if move == "down" else -int(val)) for move, val in moves
              if move == "up" or move == "down"]
-             
-    return sum(horizontal), sum(depth) 
+    return sum(horizontal), sum(depth)
+
+
+# %% Day 2 part 2
+
+from functools import reduce
+
+
+def move_aim(instructions, position=[0, 0], aim=0):
+    moves = [(inst.split(" ")[0], int(inst.split(" ")[1]))
+             for inst in instructions]
+
+    def adjust_pos(move, aim):
+        mv, val = move
+        if mv == "down":
+            aim = aim + val
+        elif mv == "up":
+            aim = aim - val
+        elif mv == "forward":
+            pass
+            #position[0] = position[0] + magnitude
+            #position[1] = position[1] + (magnitude * aim)
+        #return x, y, aim
+
+    #(reduce(adjust_pos, moves, (*position, aim)))
+
+    for move, val in moves:
+
+        if move == "down":
+            aim = aim + val
+        elif move == "up":
+            aim = aim - val
+        elif move == "forward":
+            position[0] += val  # hor
+            position[1] += val * aim  # dept
+
+    return position, aim
