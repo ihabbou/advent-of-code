b4b9f31e8cfa63d907565dfc1baaf1df95883868
ihabbou <10790679+ihabbou@users.noreply.github.com>
2021-12-31 12:28:52 +0100

    day 5 part 2

--- day5.py
+++ day5.py
@@ -5,11 +5,12 @@
 from typing import Counter
 
 
-def intesection_points(input: str):
+def prep_input(input: str) -> list:
+    return [[tuple(map(int, p.split(","))) for p in l.split(" -> ")]
+            for l in input]
 
-    pointpairs = [[tuple(map(int, p.split(","))) for p in l.split(" -> ")]
-                  for l in input]
 
+def intesection_points(pointpairs: list):
     def line_points(pair: tuple):
         (x1, y1), (x2, y2) = pair
 
@@ -22,6 +23,43 @@ def intesection_points(input: str):
         return [(x, y) for x in xrange for y in yrange]
 
     coverage = [line_points(pair) for pair in pointpairs]
+
+    coverage = [pts for line in coverage for pts in line]
+
+    ccount = Counter(coverage)
+    return {k: v for k, v in ccount.items() if v >= 2}
+
+
+# %% Day 5 part 1
+
+import math
+
+
+def intesection_points_diagonal(pointpairs: list):
+    def line_points_diagonal(pair: tuple):
+        (x1, y1), (x2, y2) = pair
+
+        if x1 != x2 and y1 != y2 and abs(x1 - x2) != abs(y2 - y1):
+            return []
+
+        if (abs(x1 - x2) == abs(y2 - y1)):  # diagonal
+            slope = int((y2 - y1) / (x2 - x1))
+
+            xrange = range(min(x1, x2), max(x1, x2) + 1)
+            yrange = range(min(y1, y2), max(y1, y2) + 1)
+            assert len(xrange) == len(
+                yrange), f"Not diagona? range= {(xrange, yrange)}"
+            sx, sy = min((x1, y1), (x2, y2), key=lambda xy: xy[0])
+            ex, ey = max((x1, y1), (x2, y2), key=lambda xy: xy[0])
+
+            return [(sx + i, sy + (i * slope)) for i in range(len(xrange))]
+
+        else:  # horizontal / vertical
+            xrange = range(min(x1, x2), max(x1, x2) + 1)
+            yrange = range(min(y1, y2), max(y1, y2) + 1)
+            return [(x, y) for x in xrange for y in yrange]
+
+    coverage = [line_points_diagonal(pair) for pair in pointpairs]
 
     coverage = [pts for line in coverage for pts in line]
 
@@ -42,6 +80,8 @@ input = """0,9 -> 5,9
 
 with open("input5.txt", "r") as f:
     input = f.readlines()
-    result = intesection_points(input)
-    print(result)
+    result = intesection_points(prep_input(input))
+    print(len(result))
+    result = intesection_points_diagonal(prep_input(input))
+    #print(result)
     print(len(result))
