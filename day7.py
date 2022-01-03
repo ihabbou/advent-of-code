165829060a4ef76c52ced353801b79f670c7840a
ihabbou <10790679+ihabbou@users.noreply.github.com>
2022-01-03 22:27:28 +0100

    day 7 part 2

--- day7.py
+++ day7.py
@@ -19,12 +19,29 @@ def crab_alignment(subs: list):
     return fuel
 
 
-## %% Day 7 part 2
+# %% Day 7 part 2
 
-input = """16,1,2,0,4,2,7,1,2,14""".splitlines()
 
-with open("input7.txt", "r") as f:
-    input = f.readlines()
-    result = crab_alignment(prep_input(input))
-    #print(result)
-    print(sum(result))
+def crab_alignment_exp(subs: list):
+
+    subs = np.array(subs)
+
+    fueling = lambda x: np.sum(np.abs(subs - x) * (np.abs(subs - x) + 1) / 2)
+
+    all_fueling = np.vectorize(fueling)(np.arange(0, len(subs)))
+
+    return np.min(all_fueling)
+
+
+def main():
+    input = """16,1,2,0,4,2,7,1,2,14""".splitlines()
+    with open("input7.txt", "r") as f:
+        input = f.readlines()
+        result = crab_alignment(prep_input(input))
+        print(sum(result))
+        result = crab_alignment_exp(prep_input(input))
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
